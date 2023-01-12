f919dce0d296f7b91ba51f2c078ca4f80381356f
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-01-12 15:49:34 -0800

    finish part 1c

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,51 +1,89 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
-    </>
-  )
-}
+// const Header = (props) => {
+//   return (
+//     <>
+//       <h1>{props.course}</h1>
+//     </>
+//   )
+// }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
+// const Content = (props) => {
+//   return (
+//     <>
+//       <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+//       <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+//       <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+//     </>
+//   )
+// }
 
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
+// const Part = (props) => {
+//   return (
+//     <p>
+//       {props.part} {props.exercises}
+//     </p>
+//   )
+// }
+
+// const Total = (props) => {
+//   return (
+//     <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises  + props.parts[2].exercises }</p>
+//   )
+// }
+
+const Display = ({counter}) => <div>{counter}</div>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // const course = 'Half Stack application development'
+  // const parts = [
+  //   {
+  //     name: 'Fundamentals of React',
+  //     exercises: 10
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exercises: 7
+  //   },
+  //   {
+  //     name: 'State of a component',
+  //     exercises: 14
+  //   }
+  // ]
+
+  // return (
+  //   <div>
+  //     <Header course={course}/>
+  //     <Content parts={parts}/>
+  //     <Total parts={parts}/>
+  //   </div>
+  // )
+
+  const [ counter, setCounter ] = useState(0) //counter is assigned 0, setCounter is assigned to a function that will be used to modify the state
+
+  // setTimeout( //function passes 2 parameters: function to increment counter, and timeout of 1 second
+  //   () => setCounter(counter + 1), //this is invoked 1 second after calling setTimeout func
+  //   1000
+  // )
+
+  const increaseByOne = () => setCounter(counter + 1) //calling a function that changes the state causes the component to rerender
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={decreaseByOne} text='minus'/>
+      <Button onClick={setToZero} text='zero'/>
     </div>
+    
   )
+  
+
+
 }
 
 export default App
